c0b6dec73d57c0093b69e68880cc9a4554ee66c8
HolyMotherOfFarming <holymotheroffarming@gmail.com>
2016-11-21 09:38:18 -0500

    Update Lock.js

--- Lock.js
+++ Lock.js
@@ -1,9 +1,8 @@
 var pw = (prompt("What is the password?"));
-var TEXT = " This is all of the text on the page."
+var TEXT = "This is all of the text on the page.  When I actually make this a website, I will put stuff of value in here and try to style it."
 
 console.log(pw);
 if (pw == "bake") {
-document.write('Hello!');
 document.write(TEXT);
 } else {
 window.open("https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=mmmmmmmmmmmmmmmmmmmmmmmm...+You+will+not");
