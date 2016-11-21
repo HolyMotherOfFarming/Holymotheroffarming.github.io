cf95df0ad9d985253d373c0e88317d4bc6e46cd9
HolyMotherOfFarming <holymotheroffarming@gmail.com>
2016-11-21 09:01:35 -0500

    Update Lock.js

--- Lock.js
+++ Lock.js
@@ -1,10 +1,10 @@
 var pw = (prompt("What is the password?"));
+var TEXT = "This is all of the text on the page."
 
 console.log(pw);
 if (pw == "bake") {
 document.write('Hello!');
-window.open("https://holymotheroffarming.github.io/hmof.html");
-window.open("https://holymotheroffarming.neocities.org")
+document.write(TEXT);
 } else {
 window.open("https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=mmmmmmmmmmmmmmmmmmmmmmmm...+You+will+not");
 };
