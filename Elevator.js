ab85a2e38469f5be89b3e36ba002314dc6bfa250
HolyMotherOfFarming <holymotheroffarming@gmail.com>
2016-11-28 09:50:36 -0500

    Update Elevator.js

--- Elevator.js
+++ Elevator.js
@@ -1,13 +1,13 @@
+<div class="elevator-button">Back to Top</div>
+
 <script>
 // Elevator script included on the page, already.
 
 window.onload = function() {
   var elevator = new Elevator({
+    element: document.querySelector('.elevator-button'),
     mainAudio: '/src/to/audio.mp3',
     endAudio: '/src/to/end-audio.mp3'
   });
 }
-
-// You can run the elevator, by calling.
-elevator.elevate();
 </script>
