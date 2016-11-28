da867a5a9622186126fbfd8abde119f53a235204
HolyMotherOfFarming <holymotheroffarming@gmail.com>
2016-11-28 09:54:04 -0500

    Update Lock.js

--- Lock.js
+++ Lock.js
@@ -1,9 +1,9 @@
 var pw = (prompt("What is the password?"));
-var TEXT = "This is all of the text on the page.  When I actually make this a website, I will put stuff of value in here and try to style it."
+var TEXT = Document.write("This is all of the text on the page.  When I actually make this a website, I will put stuff of value in here and try to style it.");
 
 console.log(pw);
 if (pw == "tyrueiwoqp") {
-document.write(TEXT);
+TEXT;
 } else {
 window.open("https://www.youtube.com/watch?v=SeOAcYk3FNc");
 window.open("https://www.youtube.com/watch?v=3BE95qqkTc0");
