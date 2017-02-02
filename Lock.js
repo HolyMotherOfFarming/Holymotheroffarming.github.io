d51e3eeb3793c1e76155099e66915d142a89d724
Vicente Basil <holymotheroffarming@gmail.com>
2017-02-02 09:18:06 -0500

    Update Lock.js

--- Lock.js
+++ Lock.js
@@ -1,9 +1,9 @@
 var pw = (prompt("What is the password?"));
-var TEXT = Document.write("This is all of the text on the page.  When I actually make this a website, I will put stuff of value in here and try to style it.");
+var t = Document.write("This is all of the text on the page.  When I actually make this a website, I will put stuff of value in here and try to style it.");
 
 console.log(pw);
 if (pw == "mmmm!!....") {
-TEXT;
+t;
 } else {
 window.open("https://www.youtube.com/watch?v=SeOAcYk3FNc");
 window.open("https://www.youtube.com/watch?v=3BE95qqkTc0");
