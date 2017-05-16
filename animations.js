bb6594d3d362c0b4fd8fd7fcca96cac4fd67c412
Vicente Basil <holymotheroffarming@gmail.com>
2017-05-16 11:12:10 -0400

    Update animations.js

--- animations.js
+++ animations.js
@@ -1,7 +1,9 @@
 var boton = document.getElementById('boton');
+var hard = document.getElementById('hard');
 
 function hideButton() {
 	boton.style.display = 'none';
+	hard.style.display = 'none';
 }
 
 boton.addEventListener('click', hideButton);
